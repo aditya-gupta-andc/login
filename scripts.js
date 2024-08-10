39a0af1ce827ad50c4bcc915457fba8d711a1434
Aditya Kumar Gupta <aditya.ae-1252@andc.du.ac.in>
2024-08-10 16:42:58 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Animation timing
+    setTimeout(() => {
+        document.querySelector('.intro-container').classList.add('fade-out');
+        setTimeout(() => {
+            document.querySelector('.intro-container').style.display = 'none';
+            document.querySelector('.login-container').style.display = 'flex';
+        }, 2000); // Match this with fade-out duration
+    }, 3000); // Duration for the intro animation
+
+    const predefinedUsers = [
+        { mobile: '9415361786', password: '9415' },
+       // { mobile: '222', password: '456' },
+       // { mobile: '333', password: '789' }
+    ];
+
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const mobile = document.getElementById('mobile').value.trim();
+        const password = document.getElementById('password').value.trim();
+        const errorMessage = document.getElementById('errorMessage');
+
+        const user = predefinedUsers.find(user => user.mobile === mobile && user.password === password);
+        if (user) {
+            sessionStorage.setItem('userMobile', mobile);
+            window.location.href = 'dashboard.html';
+        } else {
+            errorMessage.innerText = 'Invalid mobile number or password';
+            errorMessage.style.display = 'block';
+        }
+    });
+});
